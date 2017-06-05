60a75679430d03e6289d466b3ba705c5bef4e577
Bardolos <whisperdraw@gmail.com>
2017-06-05 07:35:12 -0700

    change y axis to 1 decimal place only

--- src/yAxis.js
+++ src/yAxis.js
@@ -64,7 +64,7 @@ export default class YAxis extends Component<void, any, any> {
 		}
 		minBound = (minBound < 0) ? 0 : minBound;
 		let label = minBound + (maxBound - minBound) / this.props.verticalGridStep * index;
-		label = parseFloat(label.toFixed(3));
+		label = parseFloat(label.toFixed(1));
 
 		if (!this.props.yAxisUseDecimal) {
 			label = Math.round(label);
